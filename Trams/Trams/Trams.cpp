a89070e64f3c419f2f228c84afa4cc940df15ced
Taniushka <tata06@i.ua>
2017-05-30 19:47:59 +0300

    DFS and dynamic adjacency
    Change adjacency matrix to dynamic variant
    Add function for count edges
    
    PROBLEMS: Very big memory size needed!

--- Trams/Trams/Trams.cpp
+++ Trams/Trams/Trams.cpp
@@ -1,10 +1,12 @@
 #include <iostream>
+#include <stack>
 
 using namespace std;
 
 int adj[100][100] = { 0 };
 
 void input();						//input and build adjacency matrix
+void DFS(int, int, int, int&);
 
 int main()
 {
@@ -16,6 +18,7 @@ void input()
 {
 	int n;							//count of vertices
 	int u, v;						//start and end vertices for each edge
+	int k = n - 1;
 	cin >> n;
 	for (int i = 0; i < n - 1; i++)
 	{
@@ -28,6 +31,57 @@ void input()
 	for (int i = 0; i < m; i++)
 	{
 		cin >> u >> v;				//start and end for transport line
-		//function to count need edges
+		DFS(u, v, n, k);			//function to count need edges
 	}
+}
+
+void DFS(int start, int end, int n, int &k)
+{
+	start--; end--;
+	stack<int> DFSStack;
+	int u, v;
+	bool *visited = new bool[n];
+	for (int i = 0; i < n; i++)
+	{
+		visited[i] = false;
+	}
+	DFSStack.push(start);
+	visited[start] = true;
+	while (!DFSStack.empty())
+	{
+		bool flag = false;
+		for (int i = 0; i < n; i++)
+		{
+			if (adj[DFSStack.top()][i] && !visited[i])
+			{
+				DFSStack.push(i);
+				visited[i] = true;
+				flag = true;
+				break;
+			}
+		}
+		if (!flag)
+		{
+			DFSStack.pop();
+		}
+		if (DFSStack.top() == end)
+		{
+			while (!DFSStack.empty())
+			{
+				u = DFSStack.top();
+				DFSStack.pop();
+				if (DFSStack.empty()) break;
+				v = DFSStack.top();
+				if (adj[u][v] == 1)
+				{
+					adj[u][v] = 2;
+					adj[v][u] = 2;
+					k--;
+				}
+
+			}
+			break;
+		}
+	}
+	delete visited;
 }
